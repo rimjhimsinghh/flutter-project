98b1670276f368ee784ff82552e458d0d60d9954
Rimjhim Singh <singhrimjhim2809@gmail.com>
2022-06-10 02:55:59 +0530

    05 Anonymous SignIn

---
+++ lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,38 @@
+import 'package:brew_crew/services/auth.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  final AuthService _auth = AuthService(); //instace of AuthService class
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        title: Text('Sign In to Brew Crew'),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+        child: ElevatedButton(
+          child: Text('Sign In Anon'),
+          onPressed: () async {
+            dynamic result = await _auth.signInAnon();
+            if (result == null) {
+              print('Error signing in');
+            } else {
+              print('Sign-In successful!');
+              print(result);
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -3,10 +3,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 //define all diff methods - will interact with firebase-auth
 //create an instance of firebase-auth
 class AuthService {
-  final FirebaseAuth _auth = FirebaseAuth.instance; //_ property is private
-  //                object <- instance of FirebaseAuth class(properties & methods all access)
+  
+  final FirebaseAuth _auth = FirebaseAuth.instance; //_ property is private     // object <- instance of FirebaseAuth class(properties & methods all access)
 
-  //method to sign in anonymously
+//  Method to sign in anonymously
   Future signInAnon() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
