5c738cbf22160e9da20c9f6668c82277013520d6
Rimjhim Singh <singhrimjhim2809@gmail.com>
2022-06-10 20:02:30 +0530

    Use Latest Package Provider

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('home'),
-      
+    return Scaffold(
+      backgroundColor: Colors.brown[50],
+      appBar: AppBar(
+        title: Text('Brew Crew'),
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        actions: <Widget>[
+          ElevatedButton.icon(
+              onPressed: () {}, icon: Icon(Icons.person), label: Text('Logout'))
+        ],
+      ),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew/screens/authenticate/authenticate.dart';
+import 'package:brew_crew/screens/home/home.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:brew_crew/models/user.dart';
@@ -8,8 +9,13 @@ class Wrapper extends StatelessWidget {
   Widget build(BuildContext context) {
     final user = Provider.of<User>(
         context); //  Listen inside wrapper - We can access <User> data from Provider
+    // print(user);
 
     //return Home or Authenticate
-    return Authenticate();
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
   }
 }
