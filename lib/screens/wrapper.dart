f50b3f15a9f29ac1381a524c2da033a9f44e88f6
Rimjhim Singh <singhrimjhim2809@gmail.com>
2022-06-10 17:13:27 +0530

    Provider Package

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,9 +1,14 @@
 import 'package:brew_crew/screens/authenticate/authenticate.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:brew_crew/models/user.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(
+        context); //  Listen inside wrapper - We can access <User> data from Provider
+
     //return Home or Authenticate
     return Authenticate();
   }
