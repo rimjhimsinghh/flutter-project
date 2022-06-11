1cd8e39c456dedde6e4db5ea60eba3c0425c5eb9
Rimjhim Singh <singhrimjhim2809@gmail.com>
2022-06-12 03:41:06 +0530

    Drop-down V2

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -16,63 +16,60 @@ class _SettingsFormState extends State<SettingsForm> {
   int _currentStrength;
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          Container(
-            color: Colors.pink[400],
-            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-            child: Text(
-              'Update Brew Settings',
-              style: TextStyle(
-                  fontSize: 15.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w300),
-            ),
-          ),
-          SizedBox(height: 10.0),
-          TextFormField(
-            decoration: textInputDecoration.copyWith(hintText: 'Name'),
-            validator: (val) => val.isEmpty ? 'Please enter a name' : null,
-            onChanged: (val) => setState(() => _currentName = val),
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          //dropdown
-          DropdownButtonFormField(
-            hint: Text('Sugar'),
-            value: _currentSugars,
-            isDense: true,
-            onChanged: (newValue) {
-              setState(() {
-                _currentSugars = newValue;
-              });
-              print(_currentSugars);
-            },
-            items: sugars.map((String sugar) {
-              return DropdownMenuItem<String>(
-                value: sugar,
-                child: Text('$sugar sugars'),
-              );
-            }).toList(),
-          ), //list of dropdown menu items widgets
-          //slider
-          //Button
-          ElevatedButton(
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.pink[400])),
-              onPressed: () async {
-                print(_currentName);
-                print(_currentSugars);
-                print(_currentStrength);
-              },
+    return SingleChildScrollView(
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Container(
+              color: Colors.pink[400],
+              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
               child: Text(
-                'Update',
-                style: TextStyle(color: Colors.white),
-              ))
-        ],
+                'Update Brew Settings',
+                style: TextStyle(
+                    fontSize: 15.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w300),
+              ),
+            ),
+            SizedBox(height: 10.0),
+            TextFormField(
+              decoration: textInputDecoration.copyWith(hintText: 'Name'),
+              validator: (val) => val.isEmpty ? 'Please enter a name' : null,
+              onChanged: (val) => setState(() => _currentName = val),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            //dropdown
+            DropdownButtonFormField(
+              value: _currentSugars ?? '0',
+              items: sugars.map((sugar) {
+                return DropdownMenuItem(
+                  value: sugar,
+                  child: Text('$sugar sugar(s)'),
+                );
+              }).toList(),
+              onChanged: (val) => setState(() => _currentSugars = val),
+            ),
+            //list of dropdown menu items widgets
+            //slider
+            //Button
+            ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.pink[400])),
+                onPressed: () async {
+                  print(_currentName);
+                  print(_currentSugars);
+                  print(_currentStrength);
+                },
+                child: Text(
+                  'Update',
+                  style: TextStyle(color: Colors.white),
+                ))
+          ],
+        ),
       ),
     );
   }
