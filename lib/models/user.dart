ae7ebcdce5a8ff8063b5c1b131abfa1a7c466964
Rimjhim Singh <singhrimjhim2809@gmail.com>
2022-06-12 04:16:16 +0530

    User Data Model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,5 +1,14 @@
 class User {
-  final String uid;      //property that doesn't change as user navigates between screens
-  User({this.uid});     //  set property in Constructor (named parameters)
+  final String
+      uid; //property that doesn't change as user navigates between screens
+  User({this.uid}); //  set property in Constructor (named parameters)
 }
+
 //when new instance of User class created (pass UID property)
+class UserData {
+  final String uid;
+  final String name;
+  final String sugars;
+  final int strength;
+  UserData({this.uid, this.sugars, this.strength, this.name});
+}
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,3 +1,4 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart';
 import 'package:brew_crew/models/brew.dart';
@@ -26,9 +27,24 @@ class DatabaseService {
           sugars: doc.data['sugars'] ?? '0');
     }).toList();
   }
-//get brews Stream
 
+  //userData from snapshot
+  UserData _userDataFromSnapshot(DocumentSnapshot snapshot) {
+    return UserData(
+      uid: uid,
+      name: snapshot.data['name'],
+      sugars: snapshot.data['sugars'],
+      strength: snapshot.data['strength'],
+    );
+  }
+
+//get brews Stream
   Stream<List<Brew>> get brews {
     return brewCollection.snapshots().map(_brewListFromSnapshot);
+  }
+
+// Get User Doc Stream
+  Stream<UserData> get userData {
+    return brewCollection.document(uid).snapshots().map(_userDataFromSnapshot);
   }
 }
